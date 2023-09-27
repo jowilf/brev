bec25e860094c14554f1b95ca45393047336b408
Jocelin Hounon <hounonj@gmail.com>
2023-09-27 02:21:54 -0500

    Test the Generator in KgsService

--- backend/kgs-service/src/test/java/com/brev/kgsservice/generator/GeneratorTest.java
+++ backend/kgs-service/src/test/java/com/brev/kgsservice/generator/GeneratorTest.java
@@ -1,26 +1,32 @@
 package com.brev.kgsservice.generator;
 
+import com.brev.kgsservice.config.AppConfig;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.framework.recipes.shared.SharedCount;
+import org.apache.curator.retry.RetryOneTime;
+import org.apache.curator.test.TestingServer;
+import org.apache.curator.test.Timing;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest(
         {"spring.cloud.service-registry.auto-registration.enabled=false", "spring.cloud.zookeeper.enabled=false",
-                "spring.cloud.discovery.enabled=false", "app.range-offset=5",
-        })
+                "spring.cloud.discovery.enabled=false", "spring.main.allow-bean-definition-overriding=true",
+                "app.range-offset=5",})
 @Import(GeneratorTest.Configuration.class)
 class GeneratorTest {
-
-    private static int RANGE_OFFSET = 5;
 
     @Autowired
     private SharedCount sharedCount;
@@ -28,32 +34,51 @@ class GeneratorTest {
     @Autowired
     private Generator generator;
 
+    @Autowired
+    private AppConfig appConfig;
+
     @BeforeEach
     void setUp() {
     }
 
     @Test
     void queueIsFilledAfterInitialization() {
-        when(sharedCount.getCount()).thenThrow(new IllegalAccessError());
-        for (int i = 0; i < RANGE_OFFSET; i++) System.out.println(generator.nextKey());
+        assertEquals(sharedCount.getCount(), 1);
+        for (int i = 0; i < appConfig.getRangeOffset(); i++)
+            // Empty the queue
+            generator.nextKey();
+        assertEquals(sharedCount.getCount(), 1);
     }
 
     @Test
-    void nextKey_WithAvailableKeysInQueue_ShouldReturnNextKey() {
-        when(sharedCount.getCount()).thenReturn(1);
-        for (int i = 0; i < RANGE_OFFSET; i++)
+    void nextKey_WithEmptyQueue_ShouldRequestNewRangeAndReturnNextKey() {
+        for (int i = 0; i < appConfig.getRangeOffset(); i++)
             // Empty the queue
             generator.nextKey();
         assertNotNull(generator.nextKey());
+        assertEquals(sharedCount.getCount(), 2);
     }
 
     @TestConfiguration
     static class Configuration {
-        @Bean
-        public SharedCount sharedCount() {
-            SharedCount sharedCount = Mockito.mock(SharedCount.class);
-            when(sharedCount.getCount()).thenReturn(5);
-            return sharedCount;
+
+        @Bean(destroyMethod = "close")
+        TestingServer testingServer() throws Exception {
+            return new TestingServer();
+        }
+
+        @Bean(destroyMethod = "close")
+        CuratorFramework curatorFramework(TestingServer server) {
+            Timing timing = new Timing();
+
+            CuratorFramework curatorFramework =
+                    CuratorFrameworkFactory.builder().connectString(server.getConnectString())
+                            .sessionTimeoutMs(timing.session())
+                            .connectionTimeoutMs(timing.connection())
+                            .retryPolicy(new RetryOneTime(1))
+                            .build();
+            curatorFramework.start();
+            return curatorFramework;
         }
 
     }
